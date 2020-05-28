d4d653411c071b22a3424d7017254dcb05514655
kimgura <oli999@naver.com>
2020-05-28 13:29:02 +0900

    Step01_Hello 프로젝트 추가함

---
+++ Step01_Hello/src/test/main/MainClass.java
@@ -0,0 +1,8 @@
+package test.main;
+
+public class MainClass {
+	// run(실행) 했을때 실행의 흐름이 시작되는 main 메소드 
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ Step01_Hello/src/test/main/MainClass2.java
@@ -0,0 +1,11 @@
+package test.main;
+
+public class MainClass2 {
+	//run 했을때 실행순서가 시작되는 특별한 main 메소드 
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("main 메소드가 리턴(종료) 됩니다.");
+	}
+}
