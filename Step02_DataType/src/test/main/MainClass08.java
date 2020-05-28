89ace453ff1e15caacb4010dc6e979ce0a3e2517
kimgura <oli999@naver.com>
2020-05-28 15:05:55 +0900

    Step02_DataType 에 MainClass08 추가함

---
+++ Step02_DataType/src/test/main/MainClass08.java
@@ -0,0 +1,18 @@
+package test.main;
+
+public class MainClass08 {
+	public static void main(String[] args) {
+		//String type 의 메소드 사용해 보기 
+		String str="abcde12345";
+		//문자열의 길이
+		int size=str.length();
+		//5 번째 인덱스의 문자1개(char)
+		char ch=str.charAt(5);
+		//소문자를 모두 대문자로 변환한 문자열 얻어내기 
+		String result=str.toUpperCase();
+	}
+}
+
+
+
+
