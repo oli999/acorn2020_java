cacfbfc0310765aee9caaebeb0307b1d9f2710d4
kimgura <oli999@naver.com>
2020-06-12 12:41:26 +0900

    Step15_Swing  프로젝트에 예제 추가함

---
+++ Step15_Swing/src/test/frame5/MyFrame.java
@@ -0,0 +1,57 @@
+package test.frame5;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MyFrame extends JFrame{
+	
+	//default  생성자
+	public MyFrame() {
+		//MyFrame 의 레이아웃 메니저 지정하기
+		setLayout(new FlowLayout());
+		//버튼
+		JButton sendBtn=new JButton("전송");
+		JButton removeBtn=new JButton("삭제");
+		//프레임에 버튼 추가하기 ( FlowLayout 의 영향을 받는다 )
+		add(sendBtn);
+		add(removeBtn);
+		
+		//ActionListener 인터페이스 type  의 참조값 
+		ActionListener listener=new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//System.out.println("전송 합니다~");
+				JOptionPane.showMessageDialog(MyFrame.this, "전송합니다~");
+			}
+		};
+		
+		sendBtn.addActionListener(listener);
+		//lamda 식 활용해 보기 
+		removeBtn.addActionListener((e)->{
+			JOptionPane.showMessageDialog(MyFrame.this, "삭제합니다~");
+		});
+	}
+	
+	public static void main(String[] args) {
+		//MyFrame 클래스를 이용해서 객체 생성하고 참조값을 지역변수 frame 에 담기 
+		MyFrame frame=new MyFrame();
+		//프레임의 제목 설정
+		frame.setTitle("나의 프레임");
+		//프레임을 닫으면 자동으로 프로세스가 종료 되도록 한다.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 500, 500);
+		frame.setVisible(true);
+	}
+}
+
+
+
+
+
+
---
+++ Step15_Swing/src/test/frame6/MyFrame.java
@@ -0,0 +1,65 @@
+package test.frame6;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MyFrame extends JFrame implements ActionListener{
+	
+	//default  생성자
+	public MyFrame() {
+		//MyFrame 의 레이아웃 메니저 지정하기
+		setLayout(new FlowLayout());
+		//버튼
+		JButton sendBtn=new JButton("전송");
+		JButton removeBtn=new JButton("삭제");
+		
+		//버튼에 command 등록하기(어떤 기능을 하는 버튼인지)
+		sendBtn.setActionCommand("send");
+		removeBtn.setActionCommand("remove");
+		
+		//프레임에 버튼 추가하기 ( FlowLayout 의 영향을 받는다 )
+		add(sendBtn);
+		add(removeBtn);
+	
+		//MyFrame 객체의 참조값을 전달해서 리스너 등록하기 
+		sendBtn.addActionListener(this);
+		removeBtn.addActionListener(this);
+	}
+	
+	public static void main(String[] args) {
+		//MyFrame 클래스를 이용해서 객체 생성하고 참조값을 지역변수 frame 에 담기 
+		MyFrame frame=new MyFrame();
+		//프레임의 제목 설정
+		frame.setTitle("나의 프레임");
+		//프레임을 닫으면 자동으로 프로세스가 종료 되도록 한다.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 500, 500);
+		frame.setVisible(true);
+	}
+	//ActionListener 인터페이스를 구현 해서 강제 오버라이드된 메소드 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//JOptionPane.showMessageDialog(this, "버튼을 눌렀네?");
+		
+		//애션 command 읽어오기
+		String command=e.getActionCommand();
+		
+		if(command.equals("send")) {//전송 버튼을 눌렀을때 
+			JOptionPane.showMessageDialog(this, "전송합니다.");
+		}else if(command.equals("remove")){//삭제 버튼을 눌렀을때 
+			JOptionPane.showMessageDialog(this, "삭제합니다.");
+		}
+	}
+}
+
+
+
+
+
+
