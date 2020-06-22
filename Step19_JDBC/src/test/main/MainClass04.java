81e9ec65b69787b278477a144ca9e69476299663
kimgura <oli999@naver.com>
2020-06-22 10:57:01 +0900

    Step19_JDBC 에 예제 추가함

---
+++ Step19_JDBC/src/test/main/MainClass04.java
@@ -0,0 +1,55 @@
+package test.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		/*
+		 *  2 번 회원의 이름을 원숭이 로 수정하고자 한다.
+		 */
+		//DB 연결객체를 담을 지역변수 만들기
+		Connection conn=null;
+		try {
+			//오라클 드라이버 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//접속할 DB 의 정보 @아이피주소:port번호:db이름
+			String url="jdbc:oracle:thin:@localhost:1521:xe";
+			//계정 비밀번호를 이용해서 Connection 객체의 참조값 얻어오기
+			conn=DriverManager.getConnection(url, "scott", "tiger");
+			//예외가 발생하지 않고 여기까지 실행순서가 내려오면 접속 성공이다.
+			System.out.println("Oracle DB 접속 성공");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		int num=2;
+		String name="원숭이";
+		
+		PreparedStatement pstmt=null;
+		try {
+			String sql="UPDATE member"
+					+ " SET name=?"
+					+ " WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setInt(2, num);
+			pstmt.executeUpdate();
+			System.out.println("회원 정보를 수정 했습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+			}catch(Exception e) {}
+		}
+	}
+}	
+
+
+
+
+
+
+
---
+++ Step19_JDBC/src/test/main/Quiz01.java
@@ -0,0 +1,56 @@
+package test.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Quiz01 {
+	public static void main(String[] args) {
+		/*
+		 *  2, 해골, 행신동 이라는 정보를 
+		 *  member table 에 추가 해 보세요.
+		 */
+		int num=2;
+		String name="해골";
+		String addr="행신동";
+		
+		//DB 연결객체를 담을 지역변수 만들기
+		Connection conn=null;
+		try {
+			//오라클 드라이버 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//접속할 DB 의 정보 @아이피주소:port번호:db이름
+			String url="jdbc:oracle:thin:@localhost:1521:xe";
+			//계정 비밀번호를 이용해서 Connection 객체의 참조값 얻어오기
+			conn=DriverManager.getConnection(url, "scott", "tiger");
+			//예외가 발생하지 않고 여기까지 실행순서가 내려오면 접속 성공이다.
+			System.out.println("Oracle DB 접속 성공");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		//필요한 객체의 참조값을 담을 빈 지역 변수 미리 만들기
+		PreparedStatement pstmt=null;
+		try {
+			//실행할 미완성의 sql 문
+			String sql="INSERT INTO member"
+					+ " (num, name, addr)"
+					+ " VALUES(?, ?, ?)";
+			//PreparedStatement 객체의 참조값 얻어오기
+			pstmt=conn.prepareStatement(sql);
+			// ? 에 값을 바인딩해서 미완성의 sql 문을 완성 시킨다.
+			pstmt.setInt(1, num); 
+			pstmt.setString(2, name);
+			pstmt.setString(3, addr);
+			// 실제로 수행하기
+			pstmt.executeUpdate();
+			System.out.println("회원 정보를 저장했습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				//안전하게 마무리 작업하기 
+				if(pstmt!=null)pstmt.close();
+			}catch(Exception e) {}
+		}
+	}
+}
