961443132d4c63241d3f7717c11a1f171c610098
kimgura <oli999@naver.com>
2021-05-03 17:34:12 +0900

    Step04 에 MyMain01 클래스 추가함

---
+++ Step04_Class/src/test/study/MyMain01.java
@@ -0,0 +1,11 @@
+package test.study;
+
+import test.mypac.Car;
+
+public class MyMain01 {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		Car c1=new Car();
+		Car c2=new Car();
+	}
+}
