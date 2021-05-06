a19587f2e1613f61d95130f25bc49f90d6243bab
kimgura <oli999@naver.com>
2021-05-06 11:06:09 +0900

    Step07 에 예제 추가함

---
+++ Step07_WrapperClass/src/test/study/MyMain01.java
@@ -0,0 +1,14 @@
+package test.study;
+
+public class MyMain01 {
+	public static void main(String[] args) {
+		int num1=10;
+		Integer num2=10;
+		
+		//숫자를 문자로 바꾸기 
+		String strNum=num2.toString();
+		
+		//문자를 숫자로 바꾸기
+		int num3=Integer.parseInt("999");
+	}
+}
